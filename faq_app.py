79ebf4f5d9dd94c774d69c6b43160f1d33a9beb3
植村 洸太 <k_uemura@ad-tps.com>
2025-07-24 14:15:47 +0900

    データベース元変更

--- faq_app.py
+++ faq_app.py
@@ -16,7 +16,7 @@ import json
 def get_worksheet(sheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(
-        st.secrets["gcp_service_account"], scope
+        st.secrets["GOOGLE_CREDENTIALS"], scope
     )
     client = gspread.authorize(creds)
     spreadsheet = client.open_by_key(st.secrets["spreadsheet_id"])
